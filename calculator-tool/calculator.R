bda0ad25924a42830a2198779b6e97c7b0a9de01
seaneff <steph.eaneff@gmail.com>
2024-07-30 10:55:14 -0700

    clarify documentation and tweak field names

--- calculator-tool/calculator.R
+++ calculator-tool/calculator.R
@@ -31,6 +31,7 @@ library(ggplot2) ## for plotting: https://ggplot2.tidyverse.org/
 library(scales) ## for commas on axes of plots
 library(treemap) ## for treemap visual
 library(openxlsx) ## to read in Excel files
+library(tidyr) ## for the pivot_longer function to reshape data
 
 #############################################
 ## Read in data #############################
@@ -56,10 +57,6 @@ country_scores <- read.xlsx("calculator-tool/jee3_costing_worksheet.xlsx",
 ## Format data ##############################
 #############################################
 
-## todo: remove or comment this piece out in the final calculator tool
-## for the purpose of testing, set the value of "value" to the value of "example_value" in the multipliers table
-multipliers$value <- multipliers$example_value
-
 ## treat multipliers as numeric features
 line_items$custom_multiplier_1 <- as.numeric(as.character(line_items$custom_multiplier_1))
 line_items$custom_multiplier_2 <- as.numeric(as.character(line_items$custom_multiplier_2))
@@ -68,8 +65,8 @@ line_items$custom_multiplier_2 <- as.numeric(as.character(line_items$custom_mult
 line_items$custom_multiplier_1[which(is.na(line_items$custom_multiplier_1))] <- 1
 line_items$custom_multiplier_2[which(is.na(line_items$custom_multiplier_2))] <- 1
 
-## format all default unit costs and all numeric scores as numeric variables
-unit_costs$default_value <- as.numeric(as.character(unit_costs$default_value))
+## format all unit costs and all numeric scores as numeric variables
+unit_costs$value <- as.numeric(as.character(unit_costs$value))
 line_items$score_numeric <- as.numeric(as.character(line_items$score_numeric))
 
 #######################################################
@@ -78,7 +75,7 @@ line_items$score_numeric <- as.numeric(as.character(line_items$score_numeric))
 
 line_item_costs <- line_items %>%
   ## add info on unit costs, left join so we don't accidentally lose any line items
-  left_join(unit_costs[,which(names(unit_costs) %in% c("category_sloan", "category", "subcategory", "unit_cost", "unit", "default_value"))], by = "unit_cost") %>%
+  left_join(unit_costs[,which(names(unit_costs) %in% c("category_sloan", "category", "subcategory", "unit_cost", "unit", "value"))], by = "unit_cost") %>%
   ## add info on country scores
   left_join(country_scores[,which(names(country_scores) %in% c("metric_id", "country_score_numeric"))], by = "metric_id") %>%
   ## add info on multipliers specified at the country level as a new field called "multiplier unit"
@@ -102,13 +99,13 @@ line_item_costs <- line_items %>%
   mutate(costed_y4 = include == TRUE & (score_numeric - country_score_numeric <= 3 & cost_type  == "Recurring") | (score_numeric - country_score_numeric == 4)) %>% ## items will be costed in year 4 if they meeting one of two criteria: (1) they are from years 1-3, but recurring or (2) they occur at four levels up from the initial score, assuming progress of one point up per year 
   mutate(costed_y5 = include == TRUE & (score_numeric - country_score_numeric <= 4 & cost_type  == "Recurring") | (score_numeric - country_score_numeric == 5)) %>% ## items will be costed in year 5 if they meeting one of two criteria: (1) they are from years 1-4, but recurring or (2) they occur at five levels up from the initial score, assuming progress of one point up per year (since we don't cost a five, this isn't currently possible)
   ## calculate yearly costs for a five year period
-  mutate(y1cost = ifelse(costed_y1 == TRUE, default_value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
-  mutate(y2cost = ifelse(costed_y2 == TRUE, default_value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
-  mutate(y3cost = ifelse(costed_y3 == TRUE, default_value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
-  mutate(y4cost = ifelse(costed_y4 == TRUE, default_value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
-  mutate(y5cost = ifelse(costed_y5 == TRUE, default_value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
+  mutate(y1cost = ifelse(costed_y1 == TRUE, value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
+  mutate(y2cost = ifelse(costed_y2 == TRUE, value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
+  mutate(y3cost = ifelse(costed_y3 == TRUE, value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
+  mutate(y4cost = ifelse(costed_y4 == TRUE, value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
+  mutate(y5cost = ifelse(costed_y5 == TRUE, value*custom_multiplier_1*custom_multiplier_2*administrative_multiplier_unit, 0)) %>%
   mutate(cost_5yrs = y1cost + y2cost + y3cost + y4cost + y5cost) %>%
-  select(line_item_id, include, metric_score_id, pillar, capacity, indicator, score_numeric, score_text, attribute, requirement, action, description, unit_cost, administrative_level, y1cost, y2cost, y3cost, y4cost, y5cost, cost_5yrs, cost_type, default_value, custom_multiplier_1, custom_multiplier_2, administrative_multiplier_unit, relevant_references, optional_cost, notes_assumptions, category_sloan, category, subcategory)
+  select(line_item_id, include, metric_score_id, pillar, capacity, indicator, score_numeric, score_text, attribute, requirement, action, description, unit_cost, administrative_level, y1cost, y2cost, y3cost, y4cost, y5cost, cost_5yrs, cost_type, value, custom_multiplier_1, custom_multiplier_2, administrative_multiplier_unit, relevant_references, optional_cost, notes_assumptions, category_sloan, category, subcategory)
 
 ##############################################################
 ## Calculate costs per indicator-score #######################
@@ -155,7 +152,20 @@ cost_category_costs <-
                    cost_5yrs = sum(cost_5yrs),
                    .groups = "keep") %>%
   arrange(category, desc(cost_5yrs))
-  
+
+##############################################################
+## Calculate costs per year ##################################
+##############################################################
+
+yearly_costs <-line_item_costs %>%
+  dplyr::summarize(y1cost = sum(y1cost), 
+                   y2cost = sum(y2cost),
+                   y3cost = sum(y3cost),
+                   y4cost = sum(y4cost),
+                   y5cost = sum(y5cost),
+                   cost_5yrs = sum(cost_5yrs),
+                   .groups = "keep") 
+
 ##############################################################
 ## Export data ###############################################
 ##############################################################
@@ -166,7 +176,7 @@ excel_sheets <- list("Costs per indicator" = indicator_costs,
                      "Costs per cost category" = cost_category_costs)
 
 ## export worksheet in Excel
-write.xlsx(excel_sheets, file = "calculator-tool/example_jee3_costing_results.xlsx")
+write.xlsx(excel_sheets, file = "calculator-tool/jee3_costing_results.xlsx")
 
 ##############################################################
 ## Generate graphics #########################################
@@ -176,7 +186,21 @@ write.xlsx(excel_sheets, file = "calculator-tool/example_jee3_costing_results.xl
 cost_category_costs %>%
   treemap(index = c("category", "subcategory"),
           vSize = "cost_5yrs",
-          #fontsize.labels = 1,
+          fontsize.labels = c(0,10),
           title = NA,
           vColor = "category") 
 
+## barplot of costs per year
+yearly_costs %>%
+  pivot_longer(everything()) %>%
+  filter(name != "cost_5yrs") %>%
+  mutate(name = recode(name,  
+                         "y1cost" = "Year 1",
+                         "y2cost" = "Year 2",
+                         "y3cost" = "Year 3",
+                         "y4cost" = "Year 4",
+                         "y5cost" = "Year 5")) %>%
+  ggplot(aes(x = name, y = value)) +
+  geom_bar(stat = "identity", color = "black", fill = "#134B70") +
+  labs(x = "Year", y = "Cost", title = "Yearly costs (5 years)") +
+  theme_minimal()
